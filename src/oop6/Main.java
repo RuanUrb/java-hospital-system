a4d74191ce8fe63ff4bab2e944242f79663718f3
RuanUrb <ruaurb@alunos.utfpr.edu.br>
2022-12-11 20:25:47 -0300

    First upload

---
+++ src/oop6/Main.java
@@ -0,0 +1,13 @@
+package oop6;
+
+import javax.swing.JOptionPane;
+// Andrei Fernandes Zani & Ruan Perondi Urbanjos
+// exercicio 6
+public class Main {
+
+    public static void main(String[] args) {
+        Panel panel = new Panel();
+        panel.iniciar();
+    }
+
+}
---
+++ src/oop6/Usuario.java
@@ -0,0 +1,19 @@
+
+package oop6;
+
+
+public class Usuario {
+    String username, password, email;
+    
+    public Usuario(){
+        
+    };
+    
+    public Usuario(String email, String username, String password){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    };
+    
+    
+}
