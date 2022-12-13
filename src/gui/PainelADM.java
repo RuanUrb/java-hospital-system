4762a8245ef732f8e94b3831ed0ea5d343669045
ZaniAndrei <zaniandrei1@gmail.com>
2022-12-13 02:56:43 -0300

    Implementei a pagina de cadastro do ADM, ele chama a pagina de cadastro quando clica em registrar(qualquer), nao implementei verificação

--- src/gui/PainelADM.java
+++ src/gui/PainelADM.java
@@ -34,6 +34,11 @@ public class PainelADM extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButton1.setText("Cadastrar Paciente");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Cadastrar Funcionario");
 
@@ -72,6 +77,11 @@ public class PainelADM extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        PainelCadastro cadastro = new PainelCadastro();
+        cadastro.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
